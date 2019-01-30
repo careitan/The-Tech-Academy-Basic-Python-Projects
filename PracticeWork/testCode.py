d19b65c86f29320d1018c723e939357af25683e0
Allan Reitan <careitan2000@gmail.com>
2019-01-30 14:01:17 -0800

    Function Examples
    Build Function examples from videos.

---
+++ PracticeWork/testCode.py
@@ -0,0 +1,14 @@
+mySentence = 'I love the color'
+
+color_list = {'green', 'blue', 'red', 'cyan', 'magenta'}
+
+def color_function():
+    for i in color_list:
+        print("{} {}".format(mySentence,i))
+
+
+
+
+
+color_function()
+
