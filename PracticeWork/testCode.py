f1d2dec965cf08af1a04f4766d25ab615ff03a9d
Allan Reitan <careitan2000@gmail.com>
2019-01-30 14:09:36 -0800

    Name Parameter inout
    Build Input Paramter for a calling function.

--- PracticeWork/testCode.py
+++ PracticeWork/testCode.py
@@ -1,14 +1,14 @@
-mySentence = 'I love the color'
+mySentence = 'loves the color'
 
 color_list = {'green', 'blue', 'red', 'cyan', 'magenta'}
 
-def color_function():
+def color_function(name):
     for i in color_list:
-        print("{} {}".format(mySentence,i))
+        print("{0} {1} {2}".format(name, mySentence, i))
 
 
 
 
 
-color_function()
+color_function('ALLAN')
 
