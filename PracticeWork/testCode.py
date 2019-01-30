69bf55dca495d1703e1d9cb14baa5ac610ca6ed9
Allan Reitan <careitan2000@gmail.com>
2019-01-30 15:27:43 -0800

    Built Module Import
    Build the Import Moduke code block.

--- PracticeWork/testCode.py
+++ PracticeWork/testCode.py
@@ -3,12 +3,14 @@ mySentence = 'loves the color'
 color_list = {'green', 'blue', 'red', 'cyan', 'magenta'}
 
 def color_function(name):
+    lst = []
     for i in color_list:
-        print("{0} {1} {2}".format(name, mySentence, i))
+        msg = "{0} {1} {2}".format(name, mySentence, i)
+        lst.append(msg)
+    return lst
 
 
-
-
-
-color_function('ALLAN')
+lst = color_function('ALLAN')
+for i in lst:
+    print(i)
 
