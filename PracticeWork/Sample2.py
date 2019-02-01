46ab14bb20189308ade282da188b5fef01850906
Allan Reitan <careitan2000@gmail.com>
2019-02-01 15:46:13 -0800

    Append Function
    Build Append function

---
+++ PracticeWork/Sample2.py
@@ -0,0 +1,15 @@
+.def getName(name=""):
+    name = askName(name)
+    print("Thank you, welcome {}!".format(name))
+
+
+def askName(name):
+    go = True
+    while go:
+        if name == "":
+            name = input("Please enter your name:\n>>> ")
+            if name != "":
+                go = False
+
+    return name
+
