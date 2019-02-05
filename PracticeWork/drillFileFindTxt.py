07ebc17e1f00ba476d801cd09489c885c228798d
Allan Reitan <careitan2000@gmail.com>
2019-02-05 12:47:45 -0800

    Drill Complete
    Drill Complete

--- PracticeWork/drillFileFindTxt.py
+++ PracticeWork/drillFileFindTxt.py
@@ -1,16 +1,22 @@
 from os import listdir
 from os.path import isfile, join, getmtime
-
+import time
+import datetime
 
 def getTxtFiles():
     searchDir = 'C:\\A\\'
     for file in listdir(searchDir):
         if isfile(join(searchDir,file)):
-            renderFilelisting(join(searchDir, file), getmtime(join(searchDir,file)))
+            # get the file modification time and format it for use.
+            modTimeLocal = time.localtime(getmtime(join(searchDir,file)))
 
-def renderFilelisting(fname, ttime):
-    print("{}\t{}".format(fname, ttime))
+            renderFilelistingFormatted(join(searchDir, file), modTimeLocal)
 
+# The following function requires preformstting the ttime value to local time.
+# Functionnbelow will only perform the strftime() function to arrange the date string.
+def renderFilelistingFormatted(fname, ttime):
+    if (fname.find(".txt", len(fname)-5) > -1):
+        print("{}\t{}".format(fname, time.strftime('%m/%d/%Y', ttime)))
 
 if __name__ == "__main__":
     getTxtFiles()
