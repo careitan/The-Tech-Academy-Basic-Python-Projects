30f5befacc9603870a7ca4436da59dee0207a92d
Allan Reitan <careitan2000@gmail.com>
2019-02-06 16:44:03 -0800

    Text boxes
    Build Text Boxes on the form.

--- PracticeWork/Tkinter_Tutorial.py
+++ PracticeWork/Tkinter_Tutorial.py
@@ -1,5 +1,6 @@
 
-
+import tkinter
+from tkinter import *
 
 class ParentWindow(Frame):
     def __init__ (self, master):
@@ -9,15 +10,21 @@ class ParentWindow(Frame):
         self.master.resizable(width=False, height=False)
         self.master.geometry("{}x{}".format(700,400))
         self.master.title("Learning Tkinter!")
+        self.master.config(bg='darkgray')
 
+        self.varFName = StringVar()
+        self.varLName = StringVar()
+        self.varFName.set('Bob')
+        self.varLName.set('Smith')
 
+        print(self.varFName.get())
+        print(self.varLName.get())
 
+        self.txtFName = Entry(self.master, text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtFName.pack()
 
-
-
-
-
-
+        self.txtLName = Entry(self.master, text=self.varLName, font=("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtLName.pack()
 
 
 
@@ -27,3 +34,5 @@ if __name__ == "__main__":
     root = Tk()
     App = ParentWindow(root)
     root.mainloop()
+
+
