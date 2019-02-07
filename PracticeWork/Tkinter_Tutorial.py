01d7a23c4bc2a95fdbe23a578ec95d24d1ec49fe
Allan Reitan <careitan2000@gmail.com>
2019-02-06 17:15:58 -0800

    Form buttons
    Build Form Button Submit and Cancel

--- PracticeWork/Tkinter_Tutorial.py
+++ PracticeWork/Tkinter_Tutorial.py
@@ -10,24 +10,28 @@ class ParentWindow(Frame):
         self.master.resizable(width=False, height=False)
         self.master.geometry("{}x{}".format(700,400))
         self.master.title("Learning Tkinter!")
-        self.master.config(bg='darkgray')
+        self.master.config(bg='lightgray')
 
         self.varFName = StringVar()
         self.varLName = StringVar()
-        self.varFName.set('Bob')
-        self.varLName.set('Smith')
 
-        print(self.varFName.get())
-        print(self.varLName.get())
+        self.lblFName = Label(self.master, text='First Name: ', font=("Helvetica", 16), fg='black', bg='lightgray')
+        self.lblFName.grid(row=0, column=0, padx=(30,0), pady=(30,0))
+
+        self.lblLName = Label(self.master, text='Last Name: ', font=("Helvetica", 16), fg='black', bg='lightgray')
+        self.lblLName.grid(row=1, column=0, padx=(30,0), pady=(30,0))
 
         self.txtFName = Entry(self.master, text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue')
-        self.txtFName.pack()
+        self.txtFName.grid(row=0, column=1, padx=(30,0), pady=(30,0))
 
         self.txtLName = Entry(self.master, text=self.varLName, font=("Helvetica", 16), fg='black', bg='lightblue')
-        self.txtLName.pack()
+        self.txtLName.grid(row=1, column=1, padx=(30,0), pady=(30,0))
 
+        self.btnSubmit = Button(self.master, text="Submit", width=10, height=2)
+        self.btnSubmit.grid(row=2, column=1, padx=(0,0), pady=(30,0), sticky=NE)
 
-
+        self.btnCancel = Button(self.master, text="Cancel", width=10, height=2)
+        self.btnCancel.grid(row=2, column=1, padx=(0,90), pady=(30,0), sticky=NE)
 
 
 if __name__ == "__main__":
